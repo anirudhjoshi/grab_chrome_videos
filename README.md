44d7be20a532bf20288f0990db3013983c8a4744
unknown <github@anirudhjoshi.com>
2011-07-19 19:31:02 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+get_chrome_videos
+========
+
+#### Get downloaded flash videos from Chrome's cache ####
+
+Grabs any flash videos that are in Chrome's cache, and puts it in the Videos folder of your home directory (Windows), using [Hobocopy](https://github.com/candera/hobocopy).
+
+### Usage ###
+
+Double click on:
+	
+	get_chrome_videos.bat
+
+[Hobocopy](https://github.com/candera/hobocopy) will ask for admin privileges.
+
+The files will then copy accross to your Videos folder in your home directory (Windows). Check when the program closes itself.
+
+### References ###
+
+
+
+### Copyright ###
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
+### Warranty ###
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
---
+++ get_chrome_videos.bat
@@ -0,0 +1,66 @@
+@echo off
+
+set sys=Operating system:
+set arch=Architecture:
+set copying=Copying flash video files from %USERPROFILE%\AppData\Local\Temp\ %USERPROFILE%\Videos\
+set tip=After this program closes go to this %USERPROFILE%\Videos\ folder, and view your files using VLC (http://www.videolan.org/vlc/)
+set copy=%USERPROFILE%\AppData\Local\Temp\ %USERPROFILE%\Videos\ fla*.tmp
+set pre=Hobocopy\Hobocopy-
+set suf=-bit.exe
+
+ver | find "XP" > nul
+if %ERRORLEVEL% == 0 goto xp
+
+set RegQry=HKLM\Hardware\Description\System\CentralProcessor\0
+ 
+REG.exe Query %RegQry% > checkOS.txt
+ 
+Find /i "x86" < CheckOS.txt > StringCheck.txt
+ 
+if %ERRORLEVEL% == 0 (
+	set Bit=32
+) else (
+	set Bit=64
+)
+
+ver | find "2003" > nul
+if %ERRORLEVEL% == 0 set os_ver=2003 & goto other
+
+if not exist %SystemRoot%\system32\systeminfo.exe goto undetermined_os_arch_exit
+
+systeminfo | find "OS Name" > %TEMP%\osname.txt
+for /F "usebackq delims=: tokens=2" %%i in (%TEMP%\osname.txt) do set vers=%%i
+
+echo %vers% | find "Windows 7" > nul
+if %ERRORLEVEL% == 0 set os_ver=7 & goto other
+
+echo %vers% | find "Windows Server 2008" > nul
+if %ERRORLEVEL% == 0 set os_ver=Server 2008 & goto other
+
+echo %vers% | find "Windows Vista" > nul
+if %ERRORLEVEL% == 0 set os_ver=Vista & goto other
+
+goto undetermined_os_arch_exit
+
+:xp
+echo. & echo %sys% Windows XP
+echo %arch% 32/64-bit
+echo Using Hobocopy-XP-32-bit.exe & echo.
+echo %copying% & echo.
+echo %tip% & echo.
+%pre%XP-32%suf%-bit.exe copy
+goto exit
+
+:other
+echo. & echo %sys% Windows %os_ver%
+echo %arch% %bit%-bit
+echo Using HoboCopy-W2K3-Vista-%Bit%-bit.exe & echo.
+echo %copying% & echo.
+echo %tip% & echo.
+%pre%W2K3-Vista-%Bit%%suf% %copy%
+goto exit
+
+:undetermined_os_arch_exit
+echo Can't determine your OS/architecture.
+
+:exit
